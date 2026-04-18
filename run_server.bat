0f861aebb0ece20f343ec59303233a216f03fc47
Hanshal Bobate <hanshalbobate2006@gmail.com>
2026-04-18 20:44:00 +0530

    Change server IP addresses in run_server.bat
    Updated server address information for clarity.

--- run_server.bat
+++ run_server.bat
@@ -5,8 +5,8 @@ echo starting StockMyGhar Grocery Inventory...
 echo ---------------------------------------------------
 echo.
 echo [INFO] Server will be available at:
-echo        Local:  http://127.0.0.1:5000
-echo        Mobile: http://10.55.239.242:5000
+echo        Local:  http://localhost:5000
+echo        Mobile: run ipconfig to fing your IPv4 address and then visit http://<your_1pv4_add>:5000
 echo.
 echo [HINT] Make sure your phone is on the same hotspot!
 echo ---------------------------------------------------
